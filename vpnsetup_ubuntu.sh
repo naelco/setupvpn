7d768a00542bc1a3db158395ce71db5c50efa3f5
naelco <39563361+naelco@users.noreply.github.com>
2024-09-27 23:49:55 +0900

    Update vpnsetup_ubuntu.sh

--- vpnsetup_ubuntu.sh
+++ vpnsetup_ubuntu.sh
@@ -433,7 +433,7 @@ conn shared
   dpdtimeout=300
   dpdaction=clear
   ikev2=never
-  ike=aes256-sha2;modp2048,aes128-sha2;modp2048,aes256-sha1;modp2048,aes128-sha1;modp2048
+  ike=aes256-sha2;modp2048,aes128-sha2;modp2048,aes256-sha1;modp2048,aes128-sha1;modp2048,aes256-sha2;modp1024,aes128-sha1;modp1024
   phase2alg=aes_gcm-null,aes128-sha1,aes256-sha1,aes256-sha2_512,aes128-sha2,aes256-sha2
   ikelifetime=24h
   salifetime=24h
